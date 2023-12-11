87c9d44fd4600336e7d823420a35f259ead6d560
Md. Arshad <arshadchowdhury46@gmail.com>
2023-12-12 01:14:50 +0600

    Added slider carousel to landing page

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 $(document).ready(function () {
-    // AJAX call to numbersapi
-    $.ajax({
-        url: 'http://numbersapi.com/1/30/date?json',
-        type: 'GET',
-        dataType: 'json',
-        success: function (data) {
-            $('#content').html(data.text);
-            // You can write this information to some area in the page
-            // e.g., $('#info-area').html(data.text);
-        },
-        error: function (error) {
-            console.error('Error fetching data:', error);
-        }
-    });
+  // AJAX call to numbersapi
+  console.log("Loaded");
+  //   $.ajax({
+  //     url: "http://numbersapi.com/1/30/date?json",
+  //     type: "GET",
+  //     dataType: "json",
+  //     success: function (data) {
+  //       $("#content").html(data.text);
+  //       // You can write this information to some area in the page
+  //       // e.g., $('#info-area').html(data.text);
+  //       console.log(data);
+  //     },
+  //     error: function (error) {
+  //       console.error("Error fetching data:", error);
+  //     },
+  //   });
 
-    // Uppy for image upload
-    const uppy = Uppy({
-        autoProceed: true,
-        restrictions: {
-            maxFileSize: 1000000,
-            allowedFileTypes: ['image/*']
-        }
-    }).use(Uppy.Dashboard, {
-        target: '#drag-drop-area',
-        inline: true,
-        closeModalOnClickOutside: true
-    });
+  new Swiper(".swiper", {
+    // Optional parameters
+    direction: "horizontal",
+    loop: true,
+
+    autoplay: {
+      delay: 4000, // Adjust the delay (in milliseconds) between slides
+      disableOnInteraction: false, // Set to false to continue autoplay after user interaction (e.g., slide manually)
+    },
+    effect: "coverflow",
+    speed: 2000,
+    // If we need pagination
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    // // And if we need scrollbar
+    // scrollbar: {
+    //   el: ".swiper-scrollbar",
+    // },
+  });
 });
